eac148392b880593911480e0e09f9cb9cc84440b
Eric Versaw <ericversaw22@gmail.com>
2021-07-13 19:26:24 -0400

    Random Relic Spawn Locations made
    Added a bunch of relic spawn GOs and made a script hide all of them and then unhide 15 random ones. This will be used to spawn relic prefabs on when the hunt begins and it will be random each time the player plays at this level.
    Also added a slot in DoorOpen to trigger the relic hunt to begin, but did not implement it yet.
    Also changed some folder names in the Scripts folder to make more sense.
    ALSO: forgot to mention that I closed off certain areas of player travel so that player cannot hide from where the AI's Navmesh does not travel. This was not tested thoroughly.

---
+++ Assets/Scripts/LevelFlowScripts/RelicHuntScript.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RelicHuntScript : MonoBehaviour
+{
+    GameObject RelicHub;
+    List<GameObject> RelicList = new List<GameObject>();
+    List<GameObject> ChosenList = new List<GameObject>();
+    GameObject RipperAI;
+    //bool IsHuntStart = false;
+
+    void OnEnable()
+    {
+        RipperAI = GameObject.Find("ripperAI");
+        RipperAI.SetActive(false);
+        RelicHub = GameObject.Find("RelicSpawnLocations");
+
+        foreach(GameObject HubChild in GameObject.FindGameObjectsWithTag("relicSpawn")) 
+        {
+            RelicList.Add(HubChild);
+        }
+
+        ChosenList = CreateNewList(RelicList);
+
+        foreach(GameObject point in GameObject.FindGameObjectsWithTag("relicSpawn")) 
+        {
+            point.SetActive(false);
+        }
+        foreach (GameObject point in ChosenList)
+        {
+            point.SetActive(true);
+        }
+    }
+
+    private void OnDisable()
+    {
+        RelicList.Clear();
+        ChosenList.Clear();
+    }
+
+    private List<GameObject> CreateNewList(List<GameObject> thisList) 
+    {
+        List<GameObject> randomList = new List<GameObject>();
+
+        for (int i = 0; i < 15; i++)
+        {
+            int randomIndex = Random.Range(0, thisList.Count);
+            randomList.Add(thisList[randomIndex]);
+            print(thisList[randomIndex].name);
+            thisList.RemoveAt(randomIndex);
+        }
+        print(randomList.Count);
+        return randomList;
+    }
+}
--- Assets/Scripts/environment/LockedDoor.cs
+++ Assets/Scripts/environment/LockedDoor.cs
@@ -5,4 +5,5 @@ using UnityEngine;
 public class LockedDoor : MonoBehaviour
 {
     public bool IsLocked = false;
+    public bool Puzzle2Trigger = false;
 }
